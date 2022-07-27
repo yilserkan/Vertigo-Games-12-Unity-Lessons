650240c76484591df5018ef6c411f661a3545c07
Serkan <yilserkan123@gmail.com>
2022-07-27 12:09:45 +0300

    refactor: file structure and namespace

--- Assets/TopDownShooter/Scripts/Player Controls/Movement/PlayerMovementController.cs
+++ Assets/TopDownShooter/Scripts/Player Controls/Movement/PlayerMovementController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using TopDownShooter.PlayerInput;
 using UnityEngine;
 
-namespace TopDownShooter.PlayerMovement
+namespace TopDownShooter.PlayerControls
 {
     public class PlayerMovementController : MonoBehaviour
     {
--- Assets/TopDownShooter/Scripts/Player Controls/Movement/PlayerMovementSettings.cs
+++ Assets/TopDownShooter/Scripts/Player Controls/Movement/PlayerMovementSettings.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TopDownShooter.PlayerMovement
+namespace TopDownShooter.PlayerControls
 {
     [CreateAssetMenu(menuName = "Scriptable Objects/Player/Movement Settings")]
     public class PlayerMovementSettings : ScriptableObject
